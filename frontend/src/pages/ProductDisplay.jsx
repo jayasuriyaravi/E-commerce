080867ab1400a37aee9cfda6dedd1f45681d7499
jayasuriya R <jayasuriyaravi19@gmail.com>
2024-04-21 23:35:28 +0530

    test

--- frontend/src/pages/ProductDisplay.jsx
+++ frontend/src/pages/ProductDisplay.jsx
@@ -25,6 +25,7 @@ function ProductDisplay() {
                         </div>
                     </div>
                 </div>
+                {/* test */}
                 <div className="col-md-6">
                     <h1>name</h1>
                     <div className="row">
